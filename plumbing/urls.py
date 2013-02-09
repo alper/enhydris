8f092ed5a78f31b72bf1f2eb87fde1738c9c49f4
Alper Çugun <git@alper.nl>
2013-02-09 17:18:28 +0100

    removed previous project cruft

--- plumbing/urls.py
+++ plumbing/urls.py
@@ -7,13 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^', include('watersnake.urls')),
 
-    # TODO doing registration with confirmation
-    # url(r'^', include('registration.backends.default.urls')),
-    # url(r'^', include('registration.backends.simple.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
